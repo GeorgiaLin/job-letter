1e9dae17d1a6d0935da50e1652839fc268ad41ec
GeorgiaLin <georgialin1999@gmail.com>
2023-04-30 15:35:38 -0400

    add base url

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App.js";
 import reportWebVitals from "./reportWebVitals.js";
 
+const baseUrl =
+  document.getElementsByTagName("base")[0]?.getAttribute("href") || "/";
+const rootElement = document.getElementById("root");
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router basename={baseUrl}>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById("root")
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
