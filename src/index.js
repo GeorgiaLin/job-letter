79f0ce09b16d0f07e1458854d4ff7df2ac582f28
GeorgiaLin <georgialin1999@gmail.com>
2023-04-30 14:15:59 -0400

    lol fix bug

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-// react 17.0.2
-
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.js";
+import reportWebVitals from "./reportWebVitals.js";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,3 +10,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
