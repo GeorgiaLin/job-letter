56773822d90f32783dc8d03fc8bbd8bf2aab7328
GeorgiaLin <georgialin1999@gmail.com>
2023-05-01 15:04:29 -0400

    ga4～

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Main from "./Main.js";
 import "./App.css";
 import { initGA, trackPageView } from "./analytics.js";
 
 const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    initGA("G-649VNLTLLV"); // Replace G-XXXXXXXXXX with your Measurement ID
+
+    const unlisten = Router.browserHistory.listen(() => {
+      trackPageView();
+    });
+
+    return () => {
+      unlisten(); // Clean up the listener when the component is unmounted
+    };
+  }, [location]);
   const [personalInfo, setPersonalInfo] = useState({});
 
   return (
